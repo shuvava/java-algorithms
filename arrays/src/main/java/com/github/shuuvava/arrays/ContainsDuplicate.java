980c452505fb18d2bbd4e95db512413b7c2a5781
shuvava <shuvava@users.noreply.github.com>
2021-01-24 07:05:48 -0800

    add ContainsDuplicate

---
+++ arrays/src/main/java/com/github/shuuvava/arrays/ContainsDuplicate.java
@@ -0,0 +1,67 @@
+package com.github.shuuvava.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an array of integers, find if the array contains any duplicates.
+ * <p>
+ * Your function should return true if any value appears at least twice in the array, and it should
+ * return false if every element is distinct.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3,1] Output: true Example 2:
+ * <p>
+ * Input: [1,2,3,4] Output: false Example 3:
+ * <p>
+ * Input: [1,1,1,3,3,4,3,2,4,2] Output: true
+ */
+public class ContainsDuplicate {
+
+  /**
+   * check if array has duplicates with O(n) time complexity and O(n) memory
+   *
+   * @param nums array
+   * @param <T> any type
+   * @return True if array has duplicate
+   */
+  public static <T> Boolean ArrayContainsDuplicate(ArrayList<T> nums) {
+    Set<T> map = new HashSet<>();
+    for (T item : nums) {
+      if (map.contains(item)) {
+        return true;
+      }
+      map.add(item);
+    }
+
+    return false;
+  }
+
+  /**
+   * check if array has duplicates with O(n*log(n)) time complexity and O(1) memory with data
+   * structure mutation
+   *
+   * @param nums array
+   * @param <T>  any Comparable type
+   * @return True if array has duplicate
+   */
+  public static <T extends Comparable<T>> Boolean ArrayContainsDuplicateOptimized(
+    ArrayList<T> nums) {
+    if (nums.size() < 2) {
+      return false;
+    }
+
+    Collections.sort(nums);
+
+    for (int i = 0; i < nums.size() - 1; i++) {
+      if (nums.get(i) == nums.get(i + 1)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
---
+++ arrays/src/test/java/com.github.shuuvava.arrays/ContainsDuplicateTest.java
@@ -0,0 +1,41 @@
+package com.github.shuuvava.arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class ContainsDuplicateTest extends BaseTest {
+
+  @DisplayName("Check array contains duplicate")
+  @ParameterizedTest(name = "test #{index}")
+  @CsvSource(value = {
+    "1,2,3,1 | true",
+    "1,2,3,4 | false",
+    "1,1,1,3,3,4,3,2,4,2 | true",
+  }, delimiter = '|')
+  public void CheckArray(final String arrStr, final boolean expected) {
+    ArrayList<Integer> arr = parseArray(arrStr);
+
+    var actual = ContainsDuplicate.ArrayContainsDuplicate(arr);
+
+    assertEquals(expected, actual);
+  }
+
+  @DisplayName("Check array contains duplicate optimized version")
+  @ParameterizedTest(name = "test #{index}")
+  @CsvSource(value = {
+    "1,2,3,1 | true",
+    "1,2,3,4 | false",
+    "1,1,1,3,3,4,3,2,4,2 | true",
+  }, delimiter = '|')
+  public void CheckArray2(final String arrStr, final boolean expected) {
+    ArrayList<Integer> arr = parseArray(arrStr);
+
+    var actual = ContainsDuplicate.ArrayContainsDuplicateOptimized(arr);
+
+    assertEquals(expected, actual);
+  }
+}
