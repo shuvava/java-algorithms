ce2a831c515d2b45181e8063ba8cacac1782cbcf
shuvava <shuvava@users.noreply.github.com>
2021-02-02 05:51:06 -0800

    add plus one

---
+++ arrays/src/main/java/com/github/shuuvava/arrays/PlusOneArray.java
@@ -0,0 +1,39 @@
+package com.github.shuuvava.arrays;
+
+import java.util.ArrayList;
+
+/**
+ * Given a non-empty array of decimal digits representing a non-negative integer, increment one to
+ * the integer. The digits are stored such that the most significant digit is at the head of the
+ * list, and each element in the array contains a single digit. You may assume the integer does not
+ * contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example 1: Input: digits = [1,2,3] Output: [1,2,4] Explanation: The array represents the integer
+ * 123. Example 2: Input: digits = [4,3,2,1] Output: [4,3,2,2] Explanation: The array represents the
+ * integer 4321. Example 3: Input: digits = [0] Output: [1] Constraints: 1 <= digits.length <= 100 0
+ * <= digits[i] <= 9
+ */
+public class PlusOneArray {
+
+  public static ArrayList<Integer> plusOne(ArrayList<Integer> digits) {
+    boolean shift = false;
+    for (int i = digits.size() - 1; i >= 0; i--) {
+      var val = digits.get(i);
+      if (val == 9) {
+        digits.set(i, 0);
+        shift = true;
+      } else {
+        digits.set(i, val + 1);
+        return digits;
+      }
+    }
+    if (shift) {
+      var result = new ArrayList<Integer>();
+      result.add(1);
+      result.addAll(digits);
+      return result;
+    } else {
+      return digits;
+    }
+  }
+}
---
+++ arrays/src/test/java/com.github.shuuvava.arrays/PlusOneArrayTest.java
@@ -0,0 +1,27 @@
+package com.github.shuuvava.arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PlusOneArrayTest extends BaseTest {
+  @DisplayName("Check plus one operation")
+  @ParameterizedTest(name = "test #{index}")
+  @CsvSource(value = {
+    "9 | 1, 0",
+    "1, 2, 3 | 1, 2, 4",
+    "4, 3, 2, 1 | 4, 3, 2, 2",
+    "0 | 1",
+  }, delimiter = '|')
+  public void plusOne(final String strDigits, final String strExpected) {
+    ArrayList<Integer> digits = parseArray(strDigits);
+    ArrayList<Integer> expected = parseArray(strExpected);
+
+    var actual = PlusOneArray.plusOne(digits);
+
+    assertArrayEquals(expected.toArray(), actual.toArray());
+  }
+}
