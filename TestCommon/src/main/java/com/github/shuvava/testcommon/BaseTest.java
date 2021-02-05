33632a2186aba1266c36dba731ca770b98e8a9c3
shuvava <shuvava@users.noreply.github.com>
2021-02-05 07:15:59 -0800

    move on using List interface instead of ArrayList

--- TestCommon/src/main/java/com/github/shuvava/testcommon/BaseTest.java
+++ TestCommon/src/main/java/com/github/shuvava/testcommon/BaseTest.java
@@ -1,12 +1,25 @@
 package com.github.shuvava.testcommon;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
+/*
+ * https://www.geeksforgeeks.org/array-vs-arraylist-in-java/
+ * https://www.geeksforgeeks.org/difference-between-list-and-arraylist-in-java/
+ * The List is a child interface of Collection.
+ * It is an ordered collection of objects in which duplicate values can be stored.
+ * Since List preserves the insertion order,
+ * it allows positional access and insertion of elements.
+ * List interface is implemented by the classes of
+ *     ArrayList, LinkedList, Vector and Stack.
+ */
+
 public abstract class BaseTest {
-  protected static ArrayList<ArrayList<Integer>> parseToArrayOfArray(String input) {
+
+  protected static List<List<Integer>> parseToArrayOfArray(String input) {
     if (input == null) {
-      return new ArrayList<ArrayList<Integer>>();
+      return new ArrayList<>();
     }
     return Stream
       .of(input.split(";"))
@@ -19,10 +32,10 @@ public abstract class BaseTest {
         .map(Integer::parseInt)
         .collect(ArrayList<Integer>::new, ArrayList::add, ArrayList::addAll)
       )
-      .collect(ArrayList<ArrayList<Integer>>::new, ArrayList::add, ArrayList::addAll);
+      .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
   }
 
-  protected static ArrayList<Integer> parseArray(String input) {
+  protected static List<Integer> parseArray(String input) {
     if (input == null) {
       return new ArrayList<>();
     }
@@ -32,6 +45,25 @@ public abstract class BaseTest {
       )
       .map(String::trim)
       .mapToInt(Integer::parseInt)
+      .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+  }
+
+  protected static List<List<Character>> parseArrayOfArrayOfChar(String input) {
+    if (input == null) {
+      return new ArrayList<>();
+    }
+
+    return Stream
+      .of(input.split(";"))
+      .map(String::trim)
+      .map(line -> Stream
+        .of(
+          line.split(",")
+        )
+        .map(String::trim)
+        .map(c -> c.charAt(0))
+        .collect(ArrayList<Character>::new, ArrayList::add, ArrayList::addAll)
+      )
       .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
   }
 }
